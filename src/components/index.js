c86287269fa0b66220d4839973ab3631bc28939d
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-02-26 18:51:17 +0800

    Create highlights list component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import DieComponent from './DieComponent.vue'
 import Footer from './Footer.vue'
 import Header from './Header.vue'
+import HighlightsListItem from './HighlightsListItem.vue'
 
-export default { DieComponent, Footer, Header }
+export default { DieComponent, Footer, Header, HighlightsListItem }
