e6e819a286d4178642c135ac91720240ed65e2fc
foo-dogsquared <christiangabrielarazas@gmail.com>
2019-07-15 17:00:47 +0800

    Resolve dependency security and use vue-js-modal

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import router from './router'
 import App from './App.vue'
 import './registerServiceWorker'
 import VueRouter from 'vue-router'
+import VueModal from 'vue-js-modal'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+Vue.use(VueModal)
 
 new Vue({
   router,
